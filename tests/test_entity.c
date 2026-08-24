f84a0c9d2833e7042a509ba5b1fa2fc8846146a1
Oliver <195665471+opickford@users.noreply.github.com>
2026-08-24 14:56:19 +0100

    Revert "fix: cecs_get_component on destroyed entity (#30)" (#32)
    This reverts commit 8b3c1001a765a8e1c186cc978fa05de3fd7c86c9.

--- tests/test_entity.c
+++ tests/test_entity.c
@@ -8,32 +8,19 @@
 static void test_create()
 {
     CECS* ecs = cecs_create();
-    assert(ecs != 0);
+    CECS_EntityId e = cecs_create_entity(ecs);
+
+    assert(e != INVALID_ENTITY);
 }
 
 static void test_entity()
 {
-    CECS* ecs = cecs_create();
-    CECS_EntityId e = cecs_create_entity(ecs);
-    assert(e != INVALID_ENTITY);
-}
-
-static void test_get_component_invalid_entity()
-{
-    CECS* ecs = cecs_create();
-    CECS_EntityId e = cecs_create_entity(ecs);
-    cecs_destroy_entity(ecs, e);
-
-    // TODO: note this is an invalid component id too for now.
-    void* c = cecs_get_component(ecs, e, 0);
-    assert(e == 0);
+    test_create();
 }
 
 int main()
 {
-    test_create();
     test_entity();
-    test_get_component_invalid_entity();
     return 0;
 }
 
