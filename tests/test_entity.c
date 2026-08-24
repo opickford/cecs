ab3622f3d25c77e88c762b3b4016a86588fdfe1b
Oliver Pickford <195665471+opickford@users.noreply.github.com>
2026-08-24 14:41:42 +0100

    test: add test for getting component of invalid entity.

--- tests/test_entity.c
+++ tests/test_entity.c
@@ -8,19 +8,32 @@
 static void test_create()
 {
     CECS* ecs = cecs_create();
-    CECS_EntityId e = cecs_create_entity(ecs);
-
-    assert(e != INVALID_ENTITY);
+    assert(ecs != 0);
 }
 
 static void test_entity()
 {
-    test_create();
+    CECS* ecs = cecs_create();
+    CECS_EntityId e = cecs_create_entity(ecs);
+    assert(e != INVALID_ENTITY);
+}
+
+static void test_get_component_invalid_entity()
+{
+    CECS* ecs = cecs_create();
+    CECS_EntityId e = cecs_create_entity(ecs);
+    cecs_destroy_entity(ecs, e);
+
+    // TODO: note this is an invalid component id too for now.
+    void* c = cecs_get_component(ecs, e, 0);
+    assert(e == 0);
 }
 
 int main()
 {
+    test_create();
     test_entity();
+    test_get_component_invalid_entity();
     return 0;
 }
 
